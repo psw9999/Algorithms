ad9796388c4a23e618ad33922e15b9d3603f6978
WSP99 <nosod3@naver.com>
2021-06-22 22:41:17 +0900

    Update 99P_1이될때까지.py

--- 2_Book/99P_1이될때까지.py
+++ 2_Book/99P_1이될때까지.py
@@ -1,25 +1,3 @@
-
-# 내가 짠 코드
-# 틀린 방식은 아니나 if(k > n)이 while 안에 있어서 루프를 한번 돌때마다 비교를 한번씩 더해야함.
-# 그러므로 저 조건식을 바깥쪽으로 빼는 것이 더 효율적이다.
-# n,k = map(int,input().split())
-
-# result = 0
-
-# while(1) :
-#     if n==1 :
-#         break
-#     if(k > n) :
-#         result += (n-1)
-#         break
-#     else :
-#         temp = n % k
-#         result += (temp + 1) # 1로 뺀 횟수(temp) + 나눴으므로 1을 더함.
-#         n = (n//k)
-1
-# print(result)
-
-# 다시 짠 방식
 n,k = map(int,input().split())
 
 result = 0
