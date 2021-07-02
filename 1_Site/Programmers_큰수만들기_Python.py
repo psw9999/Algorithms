21238fd9e0d29cd012fd30c24a04323c4192f40a
WSP99 <nosod3@naver.com>
2021-07-02 23:25:32 +0900

    210702

--- 1_Site/Programmers_큰수만들기_Python.py
+++ 1_Site/Programmers_큰수만들기_Python.py
@@ -1,21 +1,30 @@
-def solution(people, limit):
-    people.sort()
-    people_num = len(people)
-    result = 0
-    cur = 0
-    for i in range(len(people)-1,0,-1) :
-        #가운데 원소로 겹치는 경우 break
-        if cur == i :
-            return result + 1
-        
-        temp = people[cur] + people[i]
-        if temp <= limit :
-            people_num-=2
-            cur+=1
-        else :
-            people_num-=1
-        result += 1
-        
-        if(people_num == 0) :
-            return result
-    return result + 1
+# 코드 리뷰
+# 1. 나는 문자열을 슬라이싱하여 앞자리와 비교하여 작은 수가 없어질 때까지 덮어쓰는 방식을 사용함.
+# 2. 다른 사람의 코드는 스택을 사용하여 비교하는 수보다 작은 pop시키는 방법을 사용함.
+# 3. 문자열 슬라이싱 방식보다는 스택을 사용하여 구현하는 것이 구현 난이도와 가독성 측면에서 더 좋은 것 같다.
+
+def solution(number, k):
+    result = number[0]
+    cnt = k
+    for i in range(1,len(number)) :
+        for j in reversed(range(len(result))) :
+            if(int(result[j]) < int(number[i])) :
+                cnt-=1
+                if cnt==0 : 
+                    return result[:j] + number[i:]
+            else :
+                j+=1
+                break
+
+        result = result[:j] + number[i]
+    return result[:-cnt]
+
+# 다른 사람 코드
+# def solution(number, k):
+#     st = []
+#     for i in range(len(number)):
+#         while st and k > 0 and st[-1] < number[i]:
+#             st.pop()
+#             k -= 1
+#         st.append(number[i])
+#     return ''.join(st[:len(st) - k])
