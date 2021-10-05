4ff4f9822bf4b2b9e36fed625c8d98b93e2e21c5
WSP99 <nosod3@naver.com>
2021-10-05 21:55:13 +0900

    211005

--- 2_Book/201P_떡볶이 떡 만들기.py
+++ 2_Book/201P_떡볶이 떡 만들기.py
@@ -1,3 +1,28 @@
 
-m,n = list(map(int, input().split()))
-print(m, n)
+# 파라메트릭 서치 사용
+# 범위 내에서 조건을 만족하는 가장 큰 값을 찾으라는 최적화 문제 
+
+n,m = list(map(int, input().split(' ')))
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+
+result = 0
+while(start <= end) :
+    total = 0
+    mid = (start+end) // 2
+    for i in array :
+        if i > mid :
+            total += mid - i
+
+
+    if total < m : 
+        end = mid - 1
+    else :
+        result = mid
+        start = mid + 1
+
+    print(result)
+        
+     
