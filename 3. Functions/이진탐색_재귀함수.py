db39e03d1a9cc5763594aa5379ba2c667b16de1c
WSP99 <nosod3@naver.com>
2021-10-05 21:53:32 +0900

    Update and rename 이진트리_재귀함수.py to 이진탐색_재귀함수.py

--- 3. Functions/이진탐색_재귀함수.py
+++ 3. Functions/이진탐색_재귀함수.py
@@ -9,9 +9,9 @@ def binary_search(array, target, start, end) :
     elif start == end : 
         return None
     elif array[mid] > target : 
-        binary_search(array, target, start, mid-1)
+        return binary_search(array, target, start, mid-1)
     elif array[mid] < target : 
-        binary_search(array, target, mid+1, end)
+        return binary_search(array, target, mid+1, end)
     
 
 # 이진 탐색의 조건 : 반드시 리스트 내부가 먼저 정렬되어 있어야 한다.
