8004762168302e9eaff11b9a4b78f3b188617b36
WSP99 <nosod3@naver.com>
2021-11-09 02:21:28 +0900

    211109

---
+++ 1_Site/Baekjoon_안전 영역_Python.py
@@ -0,0 +1,43 @@
+n = int(input())
+
+maxHgt = 0
+move = [(1,0),(-1,0),(0,1),(0,-1)]
+
+array = [[0] * n for i in range(n)]
+
+result = 0
+
+for i in range(n) :
+    array[i] = list(map(int,input().split()))
+    maxHgt = max(maxHgt, max(array[i]))
+
+for i in range(0, maxHgt) :
+    tempResult = 0
+    tempArray = [[0] * n for i in range(n)]
+    stack = []
+    for y in range(n) :
+        for x in range(n) :
+            if array[y][x] > i :
+                tempArray[y][x] = 1
+    
+    for y in range(n) :
+        for x in range(n) :
+            if tempArray[y][x] == 1:
+                tempResult+=1
+                stack.append((x,y))
+                while stack :
+                    tx,ty = stack.pop()
+                    if tx < 0 or ty < 0 or tx > (n-1) or ty > (n-1) :
+                        continue
+                    if tempArray[ty][tx] == 0 :
+                        continue
+                    if tempArray[ty][tx] == 1 :
+                        tempArray[ty][tx] = 0
+                        for m in move :
+                            stack.append((tx+m[0],ty+m[1]))
+
+    result = max(result, tempResult)
+    #print(tempResult)
+
+print(result)
+    
