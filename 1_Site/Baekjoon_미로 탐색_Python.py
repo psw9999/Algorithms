00fba4c82fa598b6a7cd82cce66ebe7a092421a2
WSP99 <nosod3@naver.com>
2021-11-20 22:44:09 +0900

    211120

--- 1_Site/Baekjoon_미로 탐색_Python.py
+++ 1_Site/Baekjoon_미로 탐색_Python.py
@@ -1,25 +1,48 @@
 from collections import deque
+from itertools import combinations
+from copy import deepcopy
 
 n,m = map(int,input().split())
-graph = []
-for _ in range(n) :
-    graph.append(list(map(int, input())))
-    
 move = [(1,0),(-1,0),(0,1),(0,-1)]
 
-queue = deque()
-queue.append((0,0))
-graph[0][0] = 2
+graph = []
+virus = []
+person = []
 
-while queue :
-    x,y = queue.popleft()
-    for mv in move :
-        dx = x + mv[0]
-        dy = y + mv[1]
-        if dx < 0 or dy < 0 or dx >= m or dy >= n :
-            continue
-        if graph[dy][dx] == 1 :
-            queue.append((dx,dy))
-            graph[dy][dx] = graph[y][x] + 1
+result = 0
+
+for i in range(n) :
+    graph.append(list(map(int,input().split())))
+    for j in range(m) :
+        if graph[i][j] == 2 :
+            virus.append((j,i))
+        if graph[i][j] == 0 :
+            person.append((j,i))
             
-print(graph[n-1][m-1]-1)
+for comb in combinations(person, 3) :
+    temp_grp = deepcopy(graph)
+    for x,y in comb :
+        temp_grp[y][x] = 1
+    for i in range(n) :
+        for j in range(m) :
+            if temp_grp[i][j] == 2 :
+                queue = deque()
+                queue.append((j,i))
+                while queue :
+                    x,y = queue.popleft()
+                    for mv in move :
+                        dx = x + mv[0]
+                        dy = y + mv[1]
+                        if dx < 0 or dy < 0 or dx >= m or dy >= n :
+                            continue
+                        if temp_grp[dy][dx] == 0 :
+                            queue.append((dx,dy))
+                            temp_grp[dy][dx] = 2
+    temp_cnt = 0
+    for i in range(n) :
+        temp_cnt += temp_grp[i].count(0)
+    result = max(result, temp_cnt)
+
+print(result)
+                        
+                
