e4ae33afdde7f130ff7858346e1a1571b70993cf
WSP99 <nosod3@naver.com>
2022-06-16 14:11:01 +0900

    220616

--- 1_Site/Baekjoon_14501_퇴사_Python.py
+++ 1_Site/Baekjoon_14501_퇴사_Python.py
@@ -3,20 +3,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-DP = [0] * (N+2)
-work = [(0,0)]
+DP = [0] * (N+1)
+work = []
 for _ in range(N) :
     T,P = map(int, input().rstrip().split())
     work.append((T,P))
-work.append((0,0))
 
 result = 0
-for i in range(N+2) :
+for i in range(N-1, -1, -1) :
     T,P = work[i]
-    DP[i] = max(DP[i-1], DP[i])
-    if i + T <= N+1 :
-        DP[i+T] = max(DP[i+T], DP[i] + P)
-
-    result = max(result, DP[i])
+    if i+T <= N :
+        result = max(result, DP[i+T]+P)
+    DP[i] = result
 
 print(result)
---
+++ 1_Site/Baekjoon_16197_두 동전_Python.py
@@ -0,0 +1,54 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+move = [(1,0),(0,1),(-1,0),(0,-1)]
+N,M = map(int, input().rstrip().split())
+
+graph = []
+for _ in range(N) :
+    graph.append(list(input().rstrip()))
+
+queue = deque()
+temp = [0]
+for i in range(N) :
+    for j in range(M) :
+        if graph[i][j] == 'o' :
+            temp.append((j,i))
+queue.append(temp)
+
+# 움직인 횟수, 동전1 위치, 동전2 위치
+while queue :
+    cnt, (x1, y1), (x2, y2) = queue.popleft()
+    
+    # 10번보다 많이 눌러야 하는 경우
+    if cnt >= 10 :
+        continue
+    
+    for mx,my in move :
+        dx1,dy1,dx2,dy2 = x1 + mx, y1 + my, x2 + mx, y2 + my
+        
+        # 둘 다 맵에서 벗어난 경우
+        if (dx1 < 0 or dy1 < 0 or dx1 >= M or dy1 >= N) and (dx2 < 0 or dy2 < 0 or dx2 >= M or dy2 >= N) :
+            continue
+        
+        # 둘 중 하나만 맵에서 벗어난 경우
+        elif (dx1 < 0 or dy1 < 0 or dx1 >= M or dy1 >= N) or (dx2 < 0 or dy2 < 0 or dx2 >= M or dy2 >= N) :
+            print(cnt+1)
+            exit(0)
+        
+        # 두가지 모두 맵에서 벗어나지 않은 경우
+        else :
+            # 이동하려는 칸에 벽이 있는 경우
+            if graph[dy1][dx1] == '#' :
+                dy1,dx1 = y1,x1
+            if graph[dy2][dx2] == '#' :
+                dy2,dx2 = y2,x2
+            
+            # 이동했는데 변화가 없는 경우 무시
+            if dy1 == y1 and dx1 == x1 and dy2 == y2 and dx2 == x2 :
+                continue
+            
+            queue.append([cnt+1,(dx1,dy1),(dx2,dy2)])
+
+print(-1)
