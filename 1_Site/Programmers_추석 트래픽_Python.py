15715bbc729c8121a1f550e17ddd09e5c3f91294
WSP99 <nosod3@naver.com>
2022-09-23 18:57:11 +0900

    220923

--- 1_Site/Programmers_추석 트래픽_Python.py
+++ 1_Site/Programmers_추석 트래픽_Python.py
@@ -1,30 +1,38 @@
-def solution(n, results):
-    arrs = []
-    answer = 0
-    for i in range(0,n+1) :
-        arrs.append([])
-        arrs[i].append([])
-        arrs[i].append([])
+SECOND = 1000
+MINUTE = SECOND * 60
+HOUR = MINUTE * 60
 
-    for win,lose in results :
-        arrs[win][0].append(lose)
-        arrs[lose][1].append(win)
+INIT_TIME = 2999
+
+def timeSplit(times, dispose):
+    times = list(map(int, times.replace(".",":").split(":")))
+    endTime = INIT_TIME + (times[3] + (times[2] * SECOND) + (times[1] * MINUTE) + (times[0] * HOUR))
+    startTime = endTime - float(dispose.replace("s",""))*1000 + 1
+    return startTime, endTime
+
+def checkTime(tasks, start, end):
+    result = 0
+    for task in tasks :
+        if task[0] <= end and task[1] >= start :
+            result += 1
+    return result
     
-    
-    for wins,loses in arrs :
-        for win in wins :
-            temps = arrs[win][0]
-            for temp in temps :
-                if not temp in wins :
-                    wins.append(temp)
+
+def solution(lines):
+    tasks = []
+    # 처리시간 담기
+    for idx,line in enumerate(lines) :
+        times = line.split(" ")
+        startTime, endTime = timeSplit(times[1], times[2])
+        tasks.append((startTime, endTime))
         
-        for lose in loses :
-            temps = arrs[lose][1]
-            for temp in temps :
-                if not temp in loses :
-                    loses.append(temp)
+    result = 0
+    # 1초간 몇개의 처리시간 있는지 확인 (시작시간)
+    for task in tasks:
+        result = max(result, checkTime(tasks, task[0], task[0]+999), checkTime(tasks, task[1], task[1] + 999))
         
-        if len(wins) + len(loses) >= (n-1) :
-            answer += 1
-         
-    return answer
+    return result
+        
+            
+        
+        
