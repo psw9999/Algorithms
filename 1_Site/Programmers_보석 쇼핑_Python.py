21fecfa8c332c2eacb3dfa4961025345136dc8aa
WSP99 <nosod3@naver.com>
2022-09-23 21:14:00 +0900

    220923

--- 1_Site/Programmers_보석 쇼핑_Python.py
+++ 1_Site/Programmers_보석 쇼핑_Python.py
@@ -1,23 +1,27 @@
 from collections import defaultdict
-        
+
 def solution(gems):
-    typeCnt = len(set(gems))
-    aLeft,aRight = 0, len(gems)-1
-    left,right = 0,0
+    # 보석 종류 갯수 확인
+    number = len(set(gems))
+    current = 0
+    result = [1,len(gems)]
+    left, right = 0,0
     gemDict = defaultdict(int)
     
-    while right < len(gems) :
-        gemDict[gems[right]] += 1
+    # 인덱스 돌아다니면서 보석 갯수 확인
+    for idx, gem in enumerate(gems) :
+        gemDict[gem] += 1
         
-        if len(gemDict) == typeCnt :
-            while left <= right :
-                if gemDict[gems[left]] >= 2:
-                    gemDict[gems[left]] -= 1
-                    left += 1
-                else :
-                    if (aRight - aLeft) > (right - left) :
-                        aRight, aLeft = right, left
-                    break      
-        right += 1
+        if gemDict[gem] == 1 :
+            current += 1
+            
+        # 보석 갯수 최적화 하기
+        while gemDict[gems[left]] >= 2 :
+            gemDict[gems[left]] -= 1
+            left += 1
         
-    return [aLeft + 1, aRight + 1]
+        if current >= number :
+            if (idx - left) < (result[1] - result[0]) :
+                result = [left+1, idx+1]
+    
+    return result
