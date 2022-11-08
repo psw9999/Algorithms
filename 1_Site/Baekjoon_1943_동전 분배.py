396cfb9a13a79d1c475b8b944c7784d2b8fe19b9
WSP99 <nosod3@naver.com>
2022-11-09 01:18:54 +0900

    221109 - DP, 배낭문제

--- 1_Site/Baekjoon_1943_동전 분배.py
+++ 1_Site/Baekjoon_1943_동전 분배.py
@@ -1,8 +1,8 @@
 import sys
-import heapq
 
 input = sys.stdin.readline
 result = [False, False, False]
+
 for r in range(3) :
     N = int(input().rstrip())
     coins = []
@@ -13,53 +13,34 @@ for r in range(3) :
         coins.append((price, count))
         sumAmount += (price * count)
     
-    # 합계가 홀수라면 탈출
-    if sumAmount %2 != 0 :
-        #print(0)
+    if (sumAmount % 2) != 0 :
         continue
-
-    target = sumAmount // 2
-    coins.sort(reverse=True)
-    queue = []
     
-    for cnt in range(1, coins[0][1]+1) :
-        price = coins[0][0] * cnt
-        if target >= price :
-            heapq.heappush(queue, (price, 0))
-        else :
+    target = sumAmount // 2
+    dp = [0] * (target+1)
+    dp[0] = 1
+    
+    for price, count in coins :
+        if result[r] :
             break
         
-    flag = False
-    while queue :
-        price, index = heapq.heappop(queue)
-        if price == target :
-            flag = True
-            break
-        
-        if (index+1) >= len(coins) :
-            continue
-            
-        for i in range(index+1, len(coins)) :
-            coin_price = coins[i][0]
-            for cnt in range(1, coins[i][1]+1) :
-                temp = price + (coin_price * cnt)
-                
-                if temp >= price :
-                    heapq.heappush(queue, (temp, i))
-                
-                else :
-                    break
-        
-    if flag :
-        result[r] = True
+        for i in range(target, price-1, -1) :
+            if result[r] :
+                break
+
+            if dp[i-price] :
+                for c in range(count) :
+                    temp = i+(price*c)
+                    if temp < target :
+                        dp[temp] = 1
+                    elif temp == target :
+                        result[r] = True
+                        break
+                    else :
+                        break
 
 for r in result :
     if r :
         print(1)
     else :
         print(0)
-    
-
-    
-    
-        
