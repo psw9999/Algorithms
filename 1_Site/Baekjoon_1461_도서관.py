de84c8263f6ab129de64334d4d287c252c364e79
WSP99 <nosod3@naver.com>
2022-11-22 15:47:01 +0900

    221122 - 그리디, 정렬

---
+++ 1_Site/Baekjoon_1461_도서관.py
@@ -0,0 +1,52 @@
+import sys
+
+input = sys.stdin.readline
+global result
+
+N,M = map(int, input().rstrip().split())
+books = list(map(int, input().rstrip().split()))
+negative, positive = [], []
+result = 0
+
+for book in books :
+    if book < 0 :
+        negative.append(abs(book))
+    else :
+        positive.append(book)
+
+negative.sort()
+positive.sort()
+
+if len(negative) == 0 :
+    result += positive[-1]
+    positive = positive[:-M]
+elif len(positive) == 0 :
+    result += negative[-1]
+    negative = negative[:-M]
+else :
+    if negative[-1] > positive[-1] :
+        result += negative[-1]
+        negative = negative[:-M]
+    elif positive[-1] > negative[-1] :
+        result += positive[-1]
+        positive = positive[:-M]
+    else :
+        if len(positive) > len(negative) :
+            result += positive[-1]
+            positive = positive[:-M]
+        else :
+            result += negative[-1]
+            negative = negative[-M]
+
+def popStack(list) :
+    global result
+    while list :
+        result += (list[-1] * 2)
+        list = list[:-M]
+
+popStack(negative)
+popStack(positive)
+print(result)
+
+            
+            
