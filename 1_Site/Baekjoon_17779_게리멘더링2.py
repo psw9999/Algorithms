dbfed22a35c147711ed6945e02d9e617226fb84e
WSP99 <nosod3@naver.com>
2022-11-25 17:52:58 +0900

    221125 - 구현, 시뮬레이션

--- 1_Site/Baekjoon_17779_게리멘더링2.py
+++ 1_Site/Baekjoon_17779_게리멘더링2.py
@@ -1,70 +1,73 @@
+
 import sys
 input = sys.stdin.readline
 
-global graph, tg
+global graph, wall
 N = int(input().rstrip())
 graph = []
-graphSum = 0
-
+wall = [[False for _ in range(N)] for _ in range(N)]
+population = 0
 for _ in range(N) :
-    temp = list(map(int, input().rstrip().split()))
-    graph.append(temp)
-    graphSum += sum(temp)
+    t = list(map(int, input().rstrip().split()))
+    population += sum(t)
+    graph.append(t)
 
-tg = [[0 for _ in range(N)] for _ in range(N)]
+# 벽 세우기
+def makeWall(x, y, d1, d2, isWalled) :
+    global wall
     
-def division(minX, minY, maxX, maxY, divisions) :
-    global graph
-    sumV = 0
-     
-    for x in range(N) :
-        for y in range(N) :
-            if (x >= minX and x <= maxX) and (y >= minY and y <= maxY) :
-                divisions.add((x,y))
+    for i in range(d1+1) :
+        wall[x+i][y-i] = isWalled
+        wall[x+d2+i][y+d2-i] = isWalled
     
-    return sumV
+    for j in range(d2+1) :
+        wall[x+j][y+j] = isWalled
+        wall[x+d1+j][y-d1+j] = isWalled
 
-def search(t, startX, startY, endX, endY, divisions) :
-    global graph, tg
-    sumV = 0
+# 각 선거구 합계 구하기
+def sumLocation(x,y,d1,d2,locations) :
+    global graph, wall
     
-    for x in range(startX, endX) :
-        for y in range(startY, endY) :
-            if (x,y) in divisions :
-                continue
-            tg[x][y] = t 
-            sumV += graph[x][y]
+    # 1번 선거구
+    for i in range(x+d1) :
+        for j in range(y+1) :
+            if wall[i][j] :
+                break 
+            locations[0] += graph[i][j]
     
-    return sumV
+    # 2번 선거구
+    for i in range(x+d2+1) :
+        for j in range(N-1, y, -1) :
+            if wall[i][j] :
+                break
+            locations[1] += graph[i][j]
+    
+    # 3번 선거구
+    for i in range(x+d1, N) :
+        for j in range(y-d1+d2) :
+            if wall[i][j] :
+                break
+            locations[2] += graph[i][j]
+    
+    # 4번 선거구
+    for i in range(x+d2+1, N) :
+        for j in range(N-1, y-d1+d2-1, -1) :
+            if wall[i][j] :
+                break
+            locations[3] += graph[i][j]
             
 result = int(1e9)
 for x in range(N) :
     for y in range(N) :
         for d1 in range(1, N) :
             for d2 in range(1, N) :
-                if x+d1+d2 >= N or y-d1 < 0 or y+d2 >= N :
+                if (x+d1+d2) >= N or (y-d1) < 0 or (y+d2) >= N :
                     continue
-                maxV, minV = 0, int(1e9)
-                divisions = set()
-                tg = [[0 for _ in range(N)] for _ in range(N)]
-                values = [0,0,0,0,0]
-                values[4] += division(x,y-d1,x+d1+d2,y+d2,divisions)
-                if x == 1 and y == 3 and d1 == 2 and d2 == 2 : 
-                    print(divisions)
-                values[0] = search(1, 0,0,x+d1,y+1,divisions)
-                values[1] = search(2, 0,y+1,x+d2+1,N,divisions)
-                values[2] = search(3, x+d1,0,N,y-d1+d2,divisions)
-                values[3] = search(4, x+d2+1,y-d1+d2,N,N,divisions)
-                
-                temp = graphSum - sum(values)
-                values[4] += temp
-                if x == 1 and y == 3 and d1 == 2 and d2 == 2 : 
-                    for t in tg :
-                        print(t)
-                    print(values)
-                minV, maxV = min(values), max(values)
-                if (maxV - minV) == 17 :
-                    print(x,y,d1,d2)
-                result = min(result, (maxV - minV))
-                
+                makeWall(x,y,d1,d2,True) 
+                locations = [0] * 5
+                sumLocation(x,y,d1,d2,locations)
+                locations[4] = population - sum(locations)
+                result = min(result, max(locations) - min(locations))
+                makeWall(x,y,d1,d2,False) 
+
 print(result)
