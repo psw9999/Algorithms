acaecdc49a64818722e50b98b4e67a8ce6392619
WSP99 <nosod3@naver.com>
2022-11-25 17:52:47 +0900

    221125 - 구현, 시뮬레이션

---
+++ 1_Site/Baekjoon_2141_우체국.py
@@ -0,0 +1,12 @@
+
+import sys
+input = sys.stdin.readline
+
+X = []
+A = []
+N = int(input().rstrip())
+for _ in range(N) :
+    x,a = map(int, input().rstrip().split())
+    X.append(x)
+    A.append(a)
+
