8bb64d665bac643ef8f9679659e52ea0cae22e3a
WSP99 <nosod3@naver.com>
2023-01-16 00:42:12 +0900

    230116 - DP (개선)

--- 1_Site/Baekjoon_11049_행렬 곱셈 순서.py
+++ 1_Site/Baekjoon_11049_행렬 곱셈 순서.py
@@ -12,11 +12,10 @@ dp = [[MAX for _ in range(N)] for _ in range(N)]
 for i in range(N) :
     dp[i][i] = 0
 
-# 몇번째 대각선?
 for i in range(1, N) :
-    for j in range(N-i) :
-        
-        for k in range(i, j):
-            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + nums[i]*nums[k+1]*nums[j+1])
-        
+    for start in range(N-i) :
+        end = start+i        
+        for k in range(start, end):
+            dp[start][end] = min(dp[start][end], dp[start][k] + dp[k+1][end] + (matrixs[start][0] * matrixs[k][1] * matrixs[end][1]))
+
 print(dp[0][-1])
