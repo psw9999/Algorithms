ae9b09cfbd5b8223faa3d008bd9b658b8a8549ea
WSP99 <nosod3@naver.com>
2023-01-18 02:55:58 +0900

    230118 HashMap

--- 1_Site/Kotlin/Baekjoon_20920_영단어 암기는 괴로워/main.kt
+++ 1_Site/Kotlin/Baekjoon_20920_영단어 암기는 괴로워/main.kt
@@ -1,80 +1,40 @@
 import java.io.*
-
-data class Dictionary(
-    val count: Int,
-    val length: Int,
-    val word: String
-)
+import java.util.*
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = System.out.bufferedWriter()
+
     val (N, M) = br.readLine().split(' ').map { it.toInt() }
-    val words = hashMapOf<String, Int>()
-    val queue = mutableListOf<Dictionary>()
+    val map = hashMapOf<String, Int>()
+    val words = TreeSet<Word>()
 
     repeat(N) {
         val word = br.readLine()
         if (word.length >= M) {
-            //words[word] = words.getOrDefault(word, 0) + 1
-            words.merge(word, 1, Integer::sum)
+            map[word] = map.getOrDefault(word, 0) + 1
         }
     }
 
-    for ((word, count) in words.entries) {
-        queue.add(
-            Dictionary(
-                count = count,
-                length = word.length,
-                word = word
-            )
-        )
+    for ((key, value) in map) {
+        words.add(Word(key, value))
     }
-    queue.sortWith(compareBy<Dictionary> { -it.count }.thenByDescending { it.length }.thenBy { it.word })
-    for (dictionary in queue) {
-        bw.write("${dictionary.word}\n")
+
+    for (word in words) {
+        bw.write("${word.spelling}\n")
     }
     bw.flush()
-    bw.close()
 }
 
-//더 나은 선택지
-//import java.io.BufferedReader
-//import java.io.BufferedWriter
-//import java.io.InputStreamReader
-//import java.io.OutputStreamWriter
-//import java.util.*
-//
-//fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
-//    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-//    val tmp = readLine().split(' ')
-//    val n = tmp[0].toInt()
-//    val min = tmp[1].toInt()
-//
-//    val map = HashMap<String, Int>()
-//    repeat(n) {
-//        val line = readLine()
-//        if(line.length >= min)
-//            map[line] = map.getOrDefault(line, 0) + 1
-//    }
-//    val set = TreeSet<Word>()
-//    for(i in map) {
-//        set.add(Word(i.key, i.value))
-//    }
-//
-//    for(i in set) {
-//        bw.write("${i.spell}\n")
-//    }
-//    bw.flush()
-//}
-//
-//data class Word(val spell : String, val cnt : Int) : Comparable<Word>{
-//    override fun compareTo(other: Word): Int =
-//        if(cnt == other.cnt) {
-//            if (spell.length == other.spell.length)
-//                spell.compareTo(other.spell)
-//            else
-//                other.spell.length - spell.length
-//        } else
-//            other.cnt - cnt
-//}
+data class Word(val spelling: String, val count: Int) : Comparable<Word> {
+    override fun compareTo(other: Word): Int =
+        if (count == other.count) {
+            if (spelling.length == other.spelling.length) {
+                spelling.compareTo(other.spelling)
+            } else {
+                other.spelling.length - spelling.length
+            }
+        } else {
+            other.count - count
+        }
+}
